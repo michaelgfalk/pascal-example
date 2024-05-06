e5a3f473ad930891a8ee303db329a3900332939e
michaelgfalk <michaelgfalk@gmail.com>
2024-05-06 12:23:50 +1000

    initial

---
+++ Fibonacci.pas
@@ -0,0 +1,32 @@
+program Fibonacci;
+
+var
+    prev, this, next : integer;
+    count : integer;
+
+function NextTerm(prev, this: integer) : integer;
+
+var
+    count : integer;
+
+begin
+    count := 10;
+    NextTerm := prev + this;
+end;
+
+begin
+   writeln ('First ten Fibonacci numbers are:');
+   count := 0;
+   prev := 0;
+   this := 1;
+   repeat
+      write (this:3);
+      next := NextTerm(prev, this);
+      prev := this;
+      this := next;
+      count := count + 1;
+   until count = 10;
+   writeln;
+
+end.
+
---
+++ helloYou.pas
@@ -0,0 +1,9 @@
+program HelloYou;
+var
+    You : string;
+begin
+    write('Who are you? ');
+    readln(You);
+    writeln('Hello there, ', You, '! How are you today?');
+end.
+
